2cefbfa9e9c5ea1dc7a2bcb5c2cdb95361c38ae8
Oskar Klintrot <oskar.klintrot@gmail.com>
2014-09-15 22:18:14 +0200

    All methods created

---
+++ 1dv402-oklib08-1-3-godtycklig-lonerevision/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1dv402_oklib08_1_3_godtycklig_lonerevision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = Strings.Console_Title;
+
+            do
+            {
+                Console.Clear();
+            } while (IsContinuing());
+        }
+        //Makes the Main loop until esc is pressed
+        private static bool IsContinuing()
+        {
+            ConsoleKeyInfo esc;
+            esc = Console.ReadKey();
+            return esc.Key != ConsoleKey.Escape;
+        }
+        //private static int ReadInt(string prompt)
+        //{
+
+        //}
+        //private static int[] ReadSalaries(int count)
+        //{
+
+        //}
+        private static void ViewMessage(string message, ConsoleColor backgroundColor = ConsoleColor.Blue, ConsoleColor foregroundColor = ConsoleColor.White)
+        {
+
+        }
+        private static void ViewResult(int[] salaries)
+        {
+
+        }
+    }
+    class MyExtensions
+    {
+        //public static int Dispersion(this int[] source)
+        //{
+
+        //}
+        //private static int Median(this int[] source)
+        //{
+
+        //}
+    }
+}
